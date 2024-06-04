0f9d18bcbd178c8669b55f758673bb7d482179ee
JihongJeong <jjhh6409@gmail.com>
2024-06-04 10:26:58 +0900

    argparser update for evaluation

--- evaluation.py
+++ evaluation.py
@@ -3,20 +3,20 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import glob
 from src import CustomDataLoader
-from src import HighFreqVit
+from src.HighFreqVit import HighFreqVitClassifier
+import argparse
 
-def main():
+def main(args):
     customloader = CustomDataLoader.DataProcesser()
-    data = customloader.get_datasets(path = "./datasets", train = False)
-    batch_size = 16
+    data = customloader.get_datasets(path = args.test_folder_name, train = False)
+    batch_size = args.batch_size
     dataloader = DataLoader(dataset = data, batch_size = batch_size)
     num_datas = len(dataloader)*batch_size
     
-    model = HighFreqVit.HighFreqVitClassifier()
-    checkpoint_list = glob.glob("./checkpoints/*.pt")
-    latest_checkpoint = checkpoint_list.pop()
-    checkpoint = torch.load(latest_checkpoint)
-    print(latest_checkpoint)
+    model = HighFreqVitClassifier()
+    checkpoint = args.checkpoint
+    checkpoint = glob.glob("./checkpoints/*{checkpoint}.pt")
+    checkpoint = torch.load(checkpoint)
     model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
     
@@ -41,4 +41,14 @@ def main():
     print("Ground Truth : \n", ground_truth)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Train a HighFreqVit model")
+    parser.add_argument('--real_folder_name', type=str, default = "real", help="Path to the train dataset")
+    parser.add_argument('--fake_folder_name', type=str, default = "generated", help="Path to the train dataset")
+    parser.add_argument('--test_folder_name', type=str, default = "./datasets/test", help="Path to the test dataset")
+    parser.add_argument('--save_path', type=str, default = "./checkpoints/", help="Path to the checkpoint dataset")
+    parser.add_argument('--batch_size', type=int, default=16, help="Mini batch size")
+    parser.add_argument('--checkpoint', type=int, default=50, help="Which checkpoint to use")
+    
+    args = parser.parse_args()
+
+    main(args)
