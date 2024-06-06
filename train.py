82fdb019ed61a1159740944f84df2a6a7c0425b6
JihongJeong <jjhh6409@gmail.com>
2024-06-06 22:56:28 +0900

    check model training with test sets

--- train.py
+++ train.py
@@ -13,18 +13,20 @@ def main(args):
     decay = args.weight_decay
 
     customloader = CustomDataLoader.DataProcesser()
-    data = customloader.get_datasets(dataset_path = "./datasets/train/", real_folder_name = args.real_folder_name, fake_folder_name = args.fake_folder_name)
+    # data = customloader.get_datasets(dataset_path = "./datasets/train/", real_folder_name = args.real_folder_name, fake_folder_name = args.fake_folder_name)
     test_data = customloader.get_datasets(dataset_path = args.test_folder_name, train = False)
-    num_datas = len(data)
-    train_size = int(num_datas * 0.8)
-    validation_size = int(num_datas - train_size)
-    train_data, validation_data = random_split(data, [train_size, validation_size])
-    test_data, _ = random_split(test_data, [int(len(test_data) * 0.1), int(len(test_data)*0.9)])
+    # num_datas = len(data)
+    # train_size = int(num_datas * 0.8)
+    # validation_size = int(num_datas - train_size)
+    # train_data, validation_data = random_split(data, [train_size, validation_size])
+    train_data = test_data
+    num_train_data = len(train_data)
+    # test_data, _ = random_split(test_data, [int(len(test_data) * 0.1), int(len(test_data)*0.9)])
     
     batch_size = args.batch_size
     train_dataloader = DataLoader(dataset = train_data, batch_size = batch_size, shuffle = True)
-    validation_dataloader = DataLoader(dataset = validation_data, batch_size = batch_size, shuffle = True)
-    test_dataloader = DataLoader(dataset = test_data, batch_size = batch_size, shuffle = True )
+    # validation_dataloader = DataLoader(dataset = validation_data, batch_size = batch_size, shuffle = True)
+    # test_dataloader = DataLoader(dataset = test_data, batch_size = batch_size, shuffle = True )
     
     classifier = HighFreqVit.HighFreqVitClassifier()
     device = torch.device('cuda:0' if torch.cuda.is_available else 'cpu')
@@ -53,11 +55,11 @@ def main(args):
         
         train_acc = 0
         train_loss = None
-        test_interval = int(len(train_dataloader)/args.test_interval)
-        validation_acc = 0
-        validation_loss = None
+        # test_interval = int(len(train_dataloader)/args.testing_session)
+        # validation_acc = 0
+        # validation_loss = None
+        start_time = time.time()
         for train_idx, train_batch in enumerate(train_dataloader):
-            start_time = time.time()
             optimizer.zero_grad()
             
             img, labels = train_batch
@@ -83,53 +85,52 @@ def main(args):
                 print(f"Running time(Train) : {(time.time() - start_time):.2f}")
                 start_time = time.time()
                 
-            if train_idx%test_interval == 0:
-                with torch.no_grad():
-                    test_acc = 0
-                    for test_idx, test_batch in enumerate(test_dataloader):
-                        img, labels = test_batch
-                        labels = list(labels)
+            # if train_idx%test_interval == 0:
+            #     with torch.no_grad():
+            #         test_acc = 0
+            #         for test_idx, test_batch in enumerate(test_dataloader):
+            #             img, labels = test_batch
+            #             labels = list(labels)
                 
-                        raw_logits, logits, loss = classifier(img.to(device), labels, device)
+            #             raw_logits, logits, loss = classifier(img.to(device), labels, device)
                 
-                        for idx in range(len(labels)):
-                            # print((labels[idx], logits[idx], raw_logits[idx]))
-                            if labels[idx] == logits[idx]:
-                                test_acc = test_acc + 1
+            #             for idx in range(len(labels)):
+            #                 # print((labels[idx], logits[idx], raw_logits[idx]))
+            #                 if labels[idx] == logits[idx]:
+            #                     test_acc = test_acc + 1
                 
-                print(f"\n<Test Session> - {train_idx}/{len(train_dataloader)}th iteration : ")
-                print(f"Testset accuracy : {(test_acc/len(test_data))*100:.2f}%")
-                print(f"Running time(Test) : {(time.time() - start_time):.2f}")
-                start_time = time.time()
+            #     print(f"\n<Test Session> - {train_idx}/{len(train_dataloader)}th iteration : ")
+            #     print(f"Testset accuracy : {(test_acc/len(test_data))*100:.2f}%")
+            #     print(f"Running time(Test) : {(time.time() - start_time):.2f}")
+            #     start_time = time.time()
                 
-            
-        
-        with torch.no_grad():    
-            for val_idx, val_batch in enumerate(validation_dataloader):
-                img, labels = val_batch
-                labels = list(labels)
+
+        # with torch.no_grad():    
+        #     for val_idx, val_batch in enumerate(validation_dataloader):
+        #         img, labels = val_batch
+        #         labels = list(labels)
                 
-                raw_logits, logits, loss = classifier(img.to(device), labels, device)
+        #         raw_logits, logits, loss = classifier(img.to(device), labels, device)
                 
-                for idx in range(len(labels)):
-                    if labels[idx] == logits[idx]:
-                        validation_acc = validation_acc + 1
+        #         for idx in range(len(labels)):
+        #             if labels[idx] == logits[idx]:
+        #                 validation_acc = validation_acc + 1
                 
-                if validation_loss is None:
-                    validation_loss = loss
-                else:
-                    validation_loss = torch.mean(torch.stack([validation_loss, loss]))
+        #         if validation_loss is None:
+        #             validation_loss = loss
+        #         else:
+        #             validation_loss = torch.mean(torch.stack([validation_loss, loss]))
                     
-                if val_idx%10 == 0:
-                    print(f"\nEpoch {epoch} - {val_idx}/{len(validation_dataloader)}th iteration : ")
-                    print(f"Validation accuracy : {(validation_acc/((val_idx + 1) * batch_size)) * 100:.2f},\tValidation Loss : {validation_loss}")
+        #         if val_idx%10 == 0:
+        #             print(f"\nEpoch {epoch} - {val_idx}/{len(validation_dataloader)}th iteration : ")
+        #             print(f"Validation accuracy : {(validation_acc/((val_idx + 1) * batch_size)) * 100:.2f},\tValidation Loss : {validation_loss}")
             
             
-        train_acc = train_acc/(len(train_dataloader) * batch_size) * 100
-        validation_acc = validation_acc/(len(validation_dataloader) * batch_size) * 100
+        train_acc = (train_acc/num_train_data) * 100
+        # validation_acc = validation_acc/(len(validation_dataloader) * batch_size) * 100
         print(f"\nEpoch : {epoch}")
         print(f"Training accuracy : {train_acc:.3f}%,\tTraining Loss : {train_loss.item():.5f}")
-        print(f"Validation accuracy : {validation_acc:.3f}%,\tValidation Loss : {validation_loss.item():.5f}\n")
+        # print(f"Validation accuracy : {validation_acc:.3f}%,\tValidation Loss : {validation_loss.item():.5f}\n")
         
 
         if epoch % 10 == 0:
@@ -149,7 +150,7 @@ if __name__ == '__main__':
     parser.add_argument('--save_path', type=str, default = "./checkpoints/", help="Path to the dataset")
     parser.add_argument('--num_epochs', type=int, default=50, help="Number of epochs to train")
     parser.add_argument('--batch_size', type=int, default=16, help="Mini batch size")
-    parser.add_argument('--test_interval', type=int, default=10, help="interval of test session during training")
+    parser.add_argument('--testing_session', type=int, default=10, help="Number of test session during training")
     parser.add_argument('--use_checkpoint', type=bool, default=False, help="whether to use checkpoints or not")
     parser.add_argument('--learning_rate', type=float, default=0.005, help="Learning rate")
     parser.add_argument('--weight_decay', type=float, default=0.005, help="Weight decay")
